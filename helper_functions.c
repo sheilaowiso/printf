6489eae2f7ba1fe429210d9c2739a0b710a9ac28
MutembeiEvans <evansmutembeinjeru@gmail.com>
2022-05-23 01:00:20 +0300

    C - printf

--- helper_functions.c
+++ helper_functions.c
@@ -44,6 +44,23 @@ void write_base(char *str)
 }
 
 /**
+ * _memcpy - copy memory area
+ * @dest: Destination for copying
+ * @src: Source to copy from
+ * @n: The number of bytes to copy
+ * Return: The _memcpy() function returns a pointer to dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
  * base_len - Calculates the length for an octal number
  * @num: The number for which the length is being calculated
  * @base: Base to be calculated by
@@ -59,4 +76,3 @@ unsigned int base_len(unsigned int num, int base)
 	}
 	return (i);
 }
-
